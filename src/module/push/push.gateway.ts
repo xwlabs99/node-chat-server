2f816a0647dd7946c911b32fd3d7449001f2fc87
weixiang <920666487@qq.com>
2019-11-11 15:06:34 +0800

    修复配置

--- src/module/push/push.gateway.ts
+++ src/module/push/push.gateway.ts
@@ -46,7 +46,7 @@ export class PushGateway {
             ...pushConfig.apns
         });
         this.UmengPushClient = new UMeng({
-            
+            ...pushConfig.umeng
         });
     }
 
